996c9c2119f95c37925bf5f7d3aa32064dda5548
getGrovy <125417752+getGrovy@users.noreply.github.com>
2023-05-10 11:37:28 +0900

    230510

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+function Hello(props){
+  console.log(props)
+  return(
+    <div>
+      <h4>{props.data.name}</h4>
+      <h3>{props.data.text}</h3>
+      <h2>{props.data.author.name}</h2>
+      <h1>{props.data.author.url}</h1>
+    </div>
+  )
+  
+}
+const dataList = {
+  name:"getgrovy",
+  text:"hello",
+  author :{
+    name : "baby",
+    url : "naver.com"
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Hello data={dataList} />);
